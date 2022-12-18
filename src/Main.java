20384222acf18527886bc715000d33393f706ab0
Shubham Jadhav <sjadhav11999@gmail.com>
2022-12-18 17:34:26 +0530

    added traversing file

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,17 @@
 public class Main {
     public static void main(String[] args) {
 
-        Tree tree=new Tree(7);
+        Tree tree = new Tree(7);
         tree.insertEnhanced(5);
         tree.insertEnhanced(10);
         tree.insertEnhanced(11);
         tree.insertEnhanced(12);
-       System.out.println(tree.rootNode.rightNode.rightNode.rightNode.value);
-       System.out.println(tree.rootNode.leftNode.leftNode);
+        tree.insertEnhanced(1);
+        tree.insertEnhanced(2);
+        tree.insertEnhanced(3);
+        System.out.println(tree.find(0));
+        System.out.println(tree.find(2));
+        System.out.println(tree.rootNode.rightNode.rightNode.rightNode.value);
+        System.out.println(tree.rootNode.leftNode.leftNode);
     }
 }
